521070fa31e16e904fab6040568410ffb2a9a555
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-01-23 07:59:08 +0100

    naar boven interactie (#9)

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ hamMenu.addEventListener('click', () => {
 });
 
 
+// naar boven button
+const backToTopButton = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+backToTopButton.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth', 
+  });
+});
