e3f2ad7ff7e7d2044149b6498bb25246d288af0e
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-01-23 05:24:55 +0100

    mobile first (media queries) en css aanpassingen (#5)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// hamburger-menu
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+});
+
 
